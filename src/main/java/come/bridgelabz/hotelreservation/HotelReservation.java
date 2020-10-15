d691bef3d3adad82d1ba9f1a9117b1bf3942f1f1
Praneethachodey <praneethachodey@gmail.com>
2020-10-15 16:56:03 +0530

    [Praneetha]Added: welcome message

---
+++ src/main/java/come/bridgelabz/hotelreservation/HotelReservation.java
@@ -0,0 +1,8 @@
+package come.bridgelabz.hotelreservation;
+
+public class HotelReservation {
+
+	public static void display() {
+		System.out.println("Welcome to hotel reservation program");
+	}
+}
---
+++ src/test/java/come/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -0,0 +1,11 @@
+package come.bridgelabz.hotelreservation;
+
+import org.junit.*;
+
+public class HotelReservationTest {
+
+	@Before
+	public void displayMessage() {
+		HotelReservation.display();
+	}
+}
